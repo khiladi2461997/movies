56abf5cfd36b1f937bd87e40aa7d0b2330376e6a
khiladi2461997 <khiladi2461997@gmail.com>
2024-08-23 20:09:55 +0530

    docker 2

--- src/main/java/online/spraydxd/movies/MovieController.java
+++ src/main/java/online/spraydxd/movies/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +16,7 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping
+    @CrossOrigin(origins = "https://movie-ui-eight.vercel.app/")
     public ResponseEntity<List> getAllMovies() {
         return new ResponseEntity<List>(movieService.allMovies(), HttpStatusCode.valueOf(200));
     }
--- src/main/java/online/spraydxd/movies/ReviewController.java
+++ src/main/java/online/spraydxd/movies/ReviewController.java
@@ -14,6 +14,7 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @PostMapping
+    @CrossOrigin(origins = "https://movie-ui-eight.vercel.app/")
     public ResponseEntity<Reviews> createReview(@RequestBody Map<String, String> payload){
       return new ResponseEntity<Reviews>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatusCode.valueOf(200));
     }
