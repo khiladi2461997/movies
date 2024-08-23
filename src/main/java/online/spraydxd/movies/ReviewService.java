e31ef482bf69664758b5d79b1fe0415f1844b9a1
khiladi2461997 <khiladi2461997@gmail.com>
2024-08-23 18:01:05 +0530

    first comment

---
+++ src/main/java/online/spraydxd/movies/ReviewService.java
@@ -0,0 +1,31 @@
+package online.spraydxd.movies;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+  @Autowired
+  private ReviewRepository repository;
+
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
+  public Reviews createReview(String reviewBody, String imdbId) {
+    Reviews review = repository.insert(new Reviews(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+    mongoTemplate.update(Movie.class)
+            .matching(Criteria.where("imdbId").is(imdbId))
+            .apply(new Update().push("reviewIds").value(review.getId()))
+            .first();
+    return review;
+  }
+}
